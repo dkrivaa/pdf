7dfb52148c4b800028833c58fc5c53a5aeba8b57
dkrivaa <dannykrivaa@hotmail.com>
2024-05-30 19:51:59 +0300

    basic app

---
+++ general/google.py
@@ -0,0 +1,23 @@
+import gspread
+import json
+import base64
+import gspread
+from google.oauth2.service_account import Credentials
+import os
+
+
+# open google sheet 'googleIntegrator'
+def open_google():
+    credentials_json_string = os.environ.get('credentials_json_string')
+    credentials_json = json.loads(base64.b64decode(credentials_json_string))
+    sheet_id = os.environ.get('sheet_id')
+
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
+    creds = Credentials.from_service_account_info(credentials_json, scopes=scopes)
+    client = gspread.authorize(creds)
+
+    book = client.open_by_key(sheet_id)
+
+    return book
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import pymupdf
 
+from general import google
+
+book = google.open_google()
+book.worksheet('pdf').update_cell(1, 1, 'test')
+
