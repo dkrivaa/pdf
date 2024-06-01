471042830cd563787386ecbb28e8561d8f385b23
dkrivaa <dannykrivaa@hotmail.com>
2024-06-01 08:49:42 +0300

    basic app

--- merge.py
+++ merge.py
@@ -33,9 +33,6 @@ for file in file_list:
         for page_num in range(pdf.page_count):
             merged_pdf.insert_pdf(pdf, from_page=page_num, to_page=page_num)
 
-        # closing the current pdf_doc
-        pdf.close()
-
 # Reading and saving merged pdf into buffer
 pdf_buffer = io.BytesIO
 merged_pdf.save(pdf_buffer)
